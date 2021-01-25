b68bafa5af7fcf1c4881981410e85aa773c8f57e
LoD2aef <lod2aef@outlook.dk>
2021-01-25 15:33:25 +0100

    making login for uniconta

--- UnicontaForm.Designer.cs
+++ UnicontaForm.Designer.cs
@@ -24,11 +24,15 @@
         /// </summary>
         private void InitializeComponent() {
             this.Connection_SessionButton = new System.Windows.Forms.Button();
+            this.UsernameInput = new System.Windows.Forms.TextBox();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Connection_SessionButton
             // 
-            this.Connection_SessionButton.Location = new System.Drawing.Point(13, 13);
+            this.Connection_SessionButton.Location = new System.Drawing.Point(87, 194);
             this.Connection_SessionButton.Name = "Connection_SessionButton";
             this.Connection_SessionButton.Size = new System.Drawing.Size(150, 23);
             this.Connection_SessionButton.TabIndex = 0;
@@ -36,20 +40,62 @@
             this.Connection_SessionButton.UseVisualStyleBackColor = true;
             this.Connection_SessionButton.Click += new System.EventHandler(this.Connection_SessionButton_Click);
             // 
+            // UsernameInput
+            // 
+            this.UsernameInput.Location = new System.Drawing.Point(145, 67);
+            this.UsernameInput.Name = "UsernameInput";
+            this.UsernameInput.Size = new System.Drawing.Size(92, 20);
+            this.UsernameInput.TabIndex = 1;
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(84, 67);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.TabIndex = 3;
+            this.UsernameLabel.Text = "Username";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(84, 107);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 4;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Location = new System.Drawing.Point(145, 104);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.Size = new System.Drawing.Size(92, 20);
+            this.PasswordInput.TabIndex = 5;
+            this.PasswordInput.UseSystemPasswordChar = true;
+            // 
             // UnicontaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(345, 229);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.UsernameInput);
             this.Controls.Add(this.Connection_SessionButton);
             this.Name = "UnicontaForm";
             this.Text = "Uniconta";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button Connection_SessionButton;
+        private System.Windows.Forms.TextBox UsernameInput;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox PasswordInput;
     }
 }
--- UnicontaForm.cs
+++ UnicontaForm.cs
@@ -16,15 +16,30 @@ using Uniconta.DataModel;
 
 namespace WinFormServer {
     public partial class UnicontaForm : Form {
+        private string UserName;
+        private string PassWord;
+        private string APIGuidKey = "";
 
         public UnicontaForm() {
             InitializeComponent();
         }
 
         private void Connection_SessionButton_Click(object sender, EventArgs e) {
-
+            UnicontaConnection UnicConn = new UnicontaConnection(APITarget.Live);
+            Session UnicSess = new Session(UnicConn);
+            UserName = UsernameInput.Text;
+            PassWord = PasswordInput.Text;
+            if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(PassWord)) {
+                ErrorCodes Login = UnicSess.LoginAsync(UserName, PassWord, LoginType.API, new Guid(APIGuidKey)).Result;
+                if (Login == ErrorCodes.Succes) {
+                    MessageBox.Show("Logged in");
+                } else {
+                    MessageBox.Show("Not logged in, something wrong");
+                }
+            } else { // username or password missing
+                MessageBox.Show($"one or both missing username : {UserName} password : {PassWord}");
+                MessageBox.Show(string.Format("one or both missing username : {0} password : {1}", UserName, PassWord));
+            }
         }
-       
-       
     }
 }
